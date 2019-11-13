f0a6a00c0a1a710d611d9997b6e23bf90666ecf0
Hernani Fernandes <hernanif1@gmail.com>
2019-11-13 17:17:24 +0100

    Initial commit

---
+++ src/main/kotlin/com/monsterapps/monsterreaderapi/MonsterreaderapiApplication.kt
@@ -0,0 +1,11 @@
+package com.monsterapps.monsterreaderapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonsterreaderapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonsterreaderapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/monsterapps/monsterreaderapi/MonsterreaderapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monsterapps.monsterreaderapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonsterreaderapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
